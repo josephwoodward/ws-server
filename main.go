07d4c09435d1128937a3d7411d85322cb6cb4d02
Joseph Woodward <joseph.woodward@xeuse.com>
2023-09-07 00:39:51 +0100

    update ws implementation

--- main.go
+++ main.go
@@ -11,14 +11,29 @@ import (
 	ws "github.com/josephwoodward/go-websocket-server/websocket"
 )
 
+type server struct {
+	srv *http.Server
+	ws  *ws.WebSocket
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/ws", ws.Upgrade)
-	server := &http.Server{Addr: ":8080", Handler: mux}
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		_, err := ws.Upgrade(w, r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+	})
+
+	server := &server{
+		srv: &http.Server{Addr: ":8080", Handler: mux},
+		ws:  &ws.WebSocket{},
+	}
 
 	go func() {
 		fmt.Printf("starting server on port 8080")
-		if err := server.ListenAndServe(); err != nil {
+		if err := server.srv.ListenAndServe(); err != nil {
 			fmt.Printf("failed to start server: %s", err)
 		}
 	}()
@@ -30,7 +45,7 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := server.Shutdown(ctx); err != nil {
+	if err := server.srv.Shutdown(ctx); err != nil {
 		fmt.Printf("failed to shutdown server: %s", err)
 	}
 }
--- websocket/ws.go
+++ websocket/ws.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"crypto/sha1"
 	"encoding/base64"
+	"fmt"
 	"net"
 	"net/http"
 )
@@ -15,7 +16,11 @@ type WebSocket struct {
 	something int
 }
 
-func Upgrade(w http.ResponseWriter, r *http.Request) {
+type WsUpgradeResult struct {
+	conn net.Conn
+}
+
+func Upgrade(w http.ResponseWriter, r *http.Request) (*WsUpgradeResult, error) {
 	// Opening handshake: https://datatracker.ietf.org/doc/html/rfc6455#section-1.3
 	// GET /chat HTTP/1.1
 	// Host: server.example.com
@@ -27,6 +32,7 @@ func Upgrade(w http.ResponseWriter, r *http.Request) {
 	// Sec-WebSocket-Version: 13
 	if r.Method != "GET" {
 		http.Error(w, "request method must be a GET", http.StatusMethodNotAllowed)
+		return nil, fmt.Errorf("request method must be a GET")
 	}
 
 	if h := r.Header.Get("Upgrade"); h != "websocket" {
@@ -45,28 +51,46 @@ func Upgrade(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid value for header 'connection'", http.StatusMethodNotAllowed)
 	}
 
-	// TODO: Response
-	// HTTP/1.1 101 Switching Protocols
-	// Upgrade: websocket
-	// Connection: Upgrade
-	// Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
 	hj, ok := w.(http.Hijacker)
 	if !ok {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+
 	conn, bufrw, err := hj.Hijack()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		conn.Close()
 	}
-	_ = &WebSocket{conn, bufrw, r.Header, 1000}
 
-	w.Header().Add("Upgrade", "websocket")
-	w.Header().Add("Connection", "Upgrade")
-	w.Header().Add("Sec-WebSocket-Accept", getAcceptHash(key))
-	w.WriteHeader(http.StatusSwitchingProtocols)
+	if bufrw.Reader.Buffered() > 0 {
+		conn.Close()
+		return nil, fmt.Errorf("client data was sent before handshake completed")
+	}
+
+	var buf [1024]byte
+	p := buf[:0]
+
+	// HTTP/1.1 101 Switching Protocols
+	// Upgrade: websocket
+	// Connection: Upgrade
+	// Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
+	// From https://tools.ietf.org/html/rfc6455#section-4.2.2
+	p = append(p, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "...)
+	p = append(p, generateAcceptHash(key)...)
+
+	if _, err = conn.Write(p); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	result := &WsUpgradeResult{
+		conn: conn,
+	}
+
+	return result, nil
 }
 
-func getAcceptHash(key string) string {
+func generateAcceptHash(key string) string {
 	h := sha1.New()
 	h.Write([]byte(key))
 	h.Write([]byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
